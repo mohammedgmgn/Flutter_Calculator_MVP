f6017434e15fafaceba1a5ed4145cae719b130b9
Mohamed Abdullah <mabdullah@itgegypt.ITGDOMAIN.ITGSOLUTIONS.com>
2018-09-10 09:23:29 +0200

    add Multiplication btn

--- lib/main.dart
+++ lib/main.dart
@@ -58,13 +58,23 @@ class TipCalculator extends StatelessWidget implements CalculatorViews {
               calcPresenter.minus();
             }
         ));
+    Container MultiBtn=   new Container(
+        margin: const EdgeInsets.only(top: 60.0),
+        child:new RaisedButton(
+            child: new Text("Subtraction"),
+            onPressed: () {
+              calcPresenter = new CalcPresenter(num1, num2, this, context);
+              calcPresenter.Multiplication();
+            }
+        ));
+
 
 
 
 
     Container container = new Container(
         padding: const EdgeInsets.all(16.0),
-        child: new Column(children: [num1One, num1Tow, SummationBtn,SubBtn]));
+        child: new Column(children: [num1One, num1Tow, SummationBtn,SubBtn,MultiBtn]));
 
     AppBar appBar = new AppBar(title: new Text("Calculator"));
 
